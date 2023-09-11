47e64e5bc0a2aed315bb11099daffd8576f2f04f
Rory <yourboyrory@gmail.com>
2023-09-11 16:05:20 -0400

    working with yad, had to log off

--- ReadLogs.sh
+++ ReadLogs.sh
@@ -4,7 +4,10 @@ monitorSSH () {
     result=$(journalctl -u sshd -S "1 second ago" --no-pager) # monitor SSH Login
     if [ "$result" != "-- No entries --" ]; then
         echo "$result"
-        notify-send "Systen Monitor" "test 1"
+        selection=$(notify-send -A "View Captured Log" "Warning SSH Event Just happened!" "$result")
+        if [ "$selection" == "0" ] ; then
+             yad --no-buttons --form  --field="Captured SSH Event":TXT "$result"
+        fi
     fi
 }
 
@@ -12,12 +15,15 @@ monitorRoot () {
     result=$(journalctl -g root -S "1 second ago" --no-pager) # Root Usage Monitor
     if [ "$result" != "-- No entries --" ]; then
         echo "$result"
-        notify-send "Warning Root Event Just happened!" "$result"
+        selection=$(notify-send -A "View Captured Log" "Warning Root Event Just happened!" "$result")
+        if [ "$selection" == "0" ] ; then
+             yad --no-buttons --list --no-headers --column="test":TEXT "$result"
+        fi
     fi
 }
 
 while true; do
-    monitorSSH
-    monitorRoot
+    monitorSSH &
+    monitorRoot &
     sleep 1
 done 
