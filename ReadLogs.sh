71f1a31eac93ce974955291950a8cf308af232cf
Rory <yourboyrory@gmail.com>
2023-09-11 15:04:38 -0400

    Started work on SSH monitor

--- ReadLogs.sh
+++ ReadLogs.sh
@@ -1,6 +1,23 @@
 #!/bin/bash
 
+monitorSSH () {
+    result=$(journalctl -u sshd -S "1 second ago" --no-pager) # monitor SSH Login
+    if [ "$result" != "-- No entries --" ]; then
+        echo "$result"
+        notify-send "Systen Monitor" "test 1"
+    fi
+}
+
+monitorRoot () {
+    result=$(journalctl -g root -S "1 second ago" --no-pager) # Root Usage Monitor
+    if [ "$result" != "-- No entries --" ]; then
+        echo "$result"
+        notify-send "Warning Root Event Just happened!" "$result"
+    fi
+}
+
 while true; do
-    journalctl -g sudo -S "1 second ago" -n 1
+    monitorSSH
+    monitorRoot
     sleep 1
 done 
