182aa714ae71ad1fd139b110f540fbf76e47d524
Rory <yourboyrory@gmail.com>
2023-09-19 14:22:34 -0400

    added extract day count var

--- ExtractLogs.sh
+++ ExtractLogs.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 fileName=$(hostname)_logs_$(date +"%Y-%m-%d_%T")          # File name will be "hostname_logs_date_time.csv" 
+extractDayCount=1                                         # the amount of days to extract from the system log
 
 echo "Delimiter is ';'"                                   # Notifies about Hard Coded Delimiter
 
-echo "Extracting 7 Days of logs"
-journalctl -S "7 days ago" --no-pager > ./$fileName.csv                 # Pulls 7 days of logs
+echo "Extracting $extractDayCount Days of logs"
+journalctl -S "$extractDayCount days ago" --no-pager > ./$fileName.csv  # Pulls 7 days of logs
 echo "Separating Dates from data into 1st column"
 perl -i -pe 's/^(.{15}).{1}/\1;/g' ./$fileName.csv                      # Separates date
 echo "Separating Dates app and machine names into 2nd column"
